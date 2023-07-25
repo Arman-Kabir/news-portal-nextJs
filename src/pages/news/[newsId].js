9fa26c1d8f6107e71960d0a24688bce8fb97921b
Arman-Kabir <armankabir8@gmail.com>
2023-07-25 15:06:38 +0600

    getStaticPaths data fetch using dynamic routes

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -17,15 +17,21 @@ NewsDetailPage.getLayout = function getLayout(page) {
 };
 
 export const getStaticPaths = async () => {
-    const res = await fetch("http://localhost:5000");
-    const data = await res.json();
+    const res = await fetch("http://localhost:5000/news");
+    const newses = await res.json();
 
-    return {paths,fallback:false};
+    const paths = newses.map((news) => ({
+        params: { newsId: news.id },
+    }))
+
+    return { paths, fallback: false };
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps = async (context) => {
+    const {params} = context;
 
-    const res = await fetch("http://localhost:5000/news/1");
+    // const res = await fetch("http://localhost:5000/news/1");
+    const res = await fetch(`http://localhost:5000/news/${params.newsId}`);
     const data = await res.json();
     console.log(data);
 
